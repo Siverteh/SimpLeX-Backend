55cbffe502683b74d232e8c6004e489fec63e2e9
Sivert Espeland Husebo <siverteh@uia.no>
2024-04-11 17:12:04 +0200

    Added working wordcount

--- obj/Debug/net8.0/SimpLeX-Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpLeX-Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpLeX-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5eba19d4037af159b3588533db5fc6781f7672e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146d3f5c6e56cbded783dc75bb83e90c6f0a2d2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpLeX-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpLeX-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
