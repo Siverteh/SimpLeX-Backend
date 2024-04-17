e5e7946f685805619343602bf4ac84bb747a0e80
Sivert Espeland Husebo <siverteh@uia.no>
2024-04-17 21:39:25 +0200

    Added working image blocks

--- obj/Debug/net8.0/SimpLeX-Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpLeX-Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpLeX-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52bccb26638f6be3e01fc4972479c50f9b90a31c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac847a7b9b3b0a7f3b1c9562b57e19d1c5d454e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpLeX-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpLeX-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
