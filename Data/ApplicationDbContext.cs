c66f354e7d4a29a3ddcf6c6d92e7e4d0fbbd6679
Sivert Espeland Husebo <siverteh@uia.no>
2024-04-24 20:55:24 +0200

    Added admin controller for adding images and templates

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -24,5 +24,7 @@ namespace SimpLeX_Backend.Data
         public DbSet<Image> Images { get; set; }
         
         public DbSet<Citation> Citations { get; set; }
+        
+        public DbSet<Template> Templates { get; set; }
     }
 }
